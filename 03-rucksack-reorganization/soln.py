52385ba6f512ae6bdb8b0fcbcfc917496ee805f7
Henry Farr <hfarr@hey.com>
2022-12-02 22:17:04 -0800

    day 3 part 2

--- 03-rucksack-reorganization/soln.py
+++ 03-rucksack-reorganization/soln.py
@@ -23,13 +23,36 @@ def part1(lines):
   total = sum([ priority(shared_letter(line)) for line in lines ])
   print(f"total {total}")
 
+
+def find_common(first_line, *lines):
+
+  intersection = set(first_line)
+  for line in lines:
+    intersection.intersection_update(set(line))
+
+  # print(len(intersection), list(intersection)) # 1
+
+  return list(intersection)[0]
+
+
+def group(iterable, group_size):
+  return zip( *([iter(iterable)] * 3) )
+
+def part2(lines):
+  groups = group(lines, 3)
+  total = sum([ priority(find_common(*group)) for group in groups ])
+  print(f"total {total}")
+
 def lines(filename):
   with open(filename) as f:
-    return f.readlines()
+    return list(map(str.strip, f.readlines()))
 
 def main():
-  filename = argv[1]
-  part1(lines(filename))
+  # filename = argv[1]
+  scenario = lines(argv[1])
+
+  part1(scenario)
+  part2(scenario)
   pass
 
 if __name__ == "__main__":
