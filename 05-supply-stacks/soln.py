df4150d1cd2ba7786abea639f20c6ee983107c6b
Henry Farr <hfarr@hey.com>
2022-12-05 17:39:09 -0800

    add parsing tokens to crates, fix tokenizer

--- 05-supply-stacks/soln.py
+++ 05-supply-stacks/soln.py
@@ -5,6 +5,17 @@ import re
 import itertools
 import functools
 
+def compose(*f):
+  def binary_compose(f,g):
+    def of(x):
+      return f(g(x))
+    return of
+  identity = lambda x: x
+  return functools.reduce(binary_compose, f, identity)
+
+def curry(f):
+  return lambda a: lambda b: f(a,b)
+
 from collections import namedtuple
 # instruction_matcher = re.compile("[^\d]*(\d*)[^\d]*(\d*)[^\d]*(\d*)")
 instruction_matcher = re.compile("^move (\d*) from (\d*) to (\d*)$")
@@ -34,26 +45,44 @@ def tokenize(lines: List[str]):
 
     if current[offset] == LBRACE:
       tokens.append(current[offset+1])
-      offset += 3
+      offset += 4
     elif current[offset] == NEWLINE:
       offset = 0
       tokens.append(ENDROW)
       current = lines.pop(0)
     elif current[offset:offset+3] == BLANK:
       tokens.append(None)
-      offset += 3
+      offset += 4
     elif current[offset] == GAP:
+      # only really happens once, probably don't need blanks detection
       offset += 1
     else: # number. the BLANK/GAP machinery would work to consume the last line but we don't want None tokens
+      # actually we are done
+      lines.pop()
+      lines.pop()
+      return tokens, lines 
       tokens.append(current[offset])
       offset += 4 # last line so we are just chewing through numbers
 
   return tokens, lines
 
 def parse_crates(tokens):
-  tok = tokens.pop(0)
-  while tok != '1': # start of the numeral row
-    pass
+  rows = []
+  current_row = []
+  while len(tokens) > 0:
+    tok = tokens.pop(0)
+    if tok == ENDROW:
+      rows.insert(0, current_row)
+      current_row = []
+      continue
+    current_row.append(tok)
+  for row in rows:
+    print(len(row))
+  print(rows)
+  
+  return list(map(compose(list, curry(filter)(lambda x: x)), zip(*rows)))
+
+
   
 def parse_instruction(line: str):
   return Instruction(*map(int, instruction_matcher.match(line).groups()))
@@ -69,6 +98,10 @@ def lines(filename):
 def main():
   toks, rest = tokenize(lines(argv[1]))
   print(toks)
+  print()
+  crate_stacks = parse_crates(toks)
+  print()
+  print(crate_stacks)
   pass
 
 if __name__ == "__main__":
