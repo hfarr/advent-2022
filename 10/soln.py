f6afc44c2e2837fd409a81a3250b03c740f18871
Henry Farr <hfarr@hey.com>
2022-12-21 14:43:03 -0800

    day 10 part 1 scratch/outline

--- 10/soln.py
+++ 10/soln.py
@@ -1,8 +1,46 @@
 from sys import argv
+from typing import List, Tuple
+
+
+def parse_instructions(lines: List[str]):
+
+  instructions = []
+  for line in lines:
+    if line == "noop":
+      instructions.append( ("noop", None ) )
+    else: # addx
+      _, value = line.split()
+      instructions.append( ("addx", int(value)) )
+  return instructions
+
+  # return [ line.split() for line in lines ]
+
+def compute(instructions: List[Tuple[str, int]]):
+  register_x = 1
+  cycle = 1
+  strengths = []
+
+  while len(instructions) > 0:
+    if (cycle - 20) % 40 == 0:
+      strengths.append(cycle * register_x)
+    instr, maybe_val = instructions.pop()
+
+    cycle += 1
+
+  # modeling multi-cycle clock instructions... where we take a measurement everytime the cycle increments.
+  # well, create a hook on cycle increment
+
+  # we measure at for non-negative integers n, cycles 20 + 40n
+  for instr, maybe_val in instructions:
+    if (cycle - 20) % 40 == 0:
+      strengths.append(cycle * register_x)
+    if instr == "addx":
+      pass
 
 
 def main():
-  pass
+  lines = open(argv[1]).readlines()
+  instructions = parse_instructions(lines)
 
 if __name__ == "__main__":
   main()
