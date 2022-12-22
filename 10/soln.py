3ad5899b894a48807c5db5af07c2769ecd4374c6
Henry Farr <hfarr@hey.com>
2022-12-21 17:57:19 -0800

    debug output (unfortunately appears I am a victim of off-by-oneitis)

--- 10/soln.py
+++ 10/soln.py
@@ -11,6 +11,7 @@ def parse_instructions(lines: List[str]):
     else: # addx
       _, value = line.split()
       instructions.append( ("addx", int(value)) )
+      # print("Parsed an add with value", value, "parsed to int as", instructions[-1][1])
   return instructions
 
   # return [ line.split() for line in lines ]
@@ -35,22 +36,29 @@ def compute(instructions: List[Tuple[str, int]]):
 
   def cycle_incr():
     nonlocal cycle
+    print(f" Cycle {cycle:3} register x has", register_x, end="")
     # increment before or after we take a signal measurement?
     cycle += 1
     if (cycle - 20) % 40 == 0:
+      # print("ADDING STRENGTH at cycle", cycle)
       strengths.append(cycle * register_x)
+      print(". Strength", cycle * register_x)
+    else:
+      print()
 
   while len(instructions) > 0:
-    if (cycle - 20) % 40 == 0:
-      strengths.append(cycle * register_x)
-    instr, maybe_val = instructions.pop()
+    instr, maybe_val = instructions.pop(0)
     if instr == "noop":
+      print("noop    ", end="")
       cycle_incr()
     elif instr == "addx":
+      print(f"addx {maybe_val:3}", end="")
       cycle_incr()
+      print("   |    ", end="")
       cycle_incr()
       # we simulate the side effect of "addx" taking effect (updating the value)
       # happening after the cycle changes. So during the two cycles of an add, the effect is non present
+      # print("Adding", maybe_val)
       register_x += maybe_val
   
   print(strengths)
@@ -68,7 +76,7 @@ def compute(instructions: List[Tuple[str, int]]):
   #     pass
 
 def part_1(signals):
-  return sum(signals[:6])
+  return sum(signals)
 
 
 def main():
@@ -76,6 +84,9 @@ def main():
   instructions = parse_instructions(lines)
   answer_1 = part_1(compute(instructions))
   print("Part 1", answer_1) # 2580, which is too low
+  # attempt 2, 10380. still too low. I was reading instructions backwards in part 1 (needed to pop from beginning)
+  # attempt 3, 14060. I was adding in values at the wrong time, ugh. it's wrong but they won't tell me too low or too high :(
+    # but it is correct for someone else
 
 if __name__ == "__main__":
   main()
